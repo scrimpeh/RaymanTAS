7331b0eacb636f70f74c3be978e0338784aa8977
scrimpeh <scrimpeh@users.noreply.github.com>
2016-08-09 23:31:32 +0200

    Improve ringboost script

--- RingBoosts.lua
+++ RingBoosts.lua
@@ -1,63 +1,62 @@
--- Lua Script by Got4n/Scrimpeh
--- Controls Rayman speed during a boost
+-- Lua Script by Got4n/scrimpeh
+-- Checks Rayman speed during a boost
 -- Launch it two frames before you get the boost (I think? pls scrimpeh confirm it)
 -- Addresses
-X_ADDR = 0x1F61BC;
-Y_ADDR = 0x1F61BE;
-COND_ADDR = 0x1F61CC;
+
+-- Constants
+RAYMAN_X = 0x1F61BC;
+RAYMAN_Y = 0x1F61BE;
+
+CAMERA_X = 0x1F84B8;
+CAMERA_Y = 0x1F84C0;
+
+X_SPEED = 0x1F61CC;
+Y_SPEED = 0x1F61CE;
  
--- Control values; number of frames to advance and how many times to test
-NUM_FRAMES = 30;
-NUM_ITER = 1000;
- 
--- Sets the RNG to val; assumes a 16-bit RNG
-function set_X(val)
-    mainmemory.write_u16_le(X_ADDR,val);
+function set_xy(x, y)
+-- Set Rayman Position
+	mainmemory.write_u16_le(RAYMAN_X, x);
+	mainmemory.write_u16_le(RAYMAN_Y, y);
+-- Write Camera
+	mainmemory.write_u16_le(CAMERA_X, x - 96);
+	mainmemory.write_u16_le(CAMERA_Y, y - 96);
 end
  
- -- Sets the RNG to val; assumes a 16-bit RNG
-function set_Y(val)
-    mainmemory.write_u16_le(Y_ADDR,val);
-end
- 
--- Function that checks whether the condition and reports whatever information
--- you are trying to track from it.
-function check_cond()
- 
-    cond = mainmemory.read_u16_le(COND_ADDR);
-    if(cond ~= 5) then
-        mycount = mycount+1;
-    end;
-   
+function check_result()
+	-- TODO: Do something useful here!
+	-- -scrimpeh
 end
  
 function advance_frames(val)
-    for i=0,val,1 do
-        emu.frameadvance();
-    end
+	for i=0,val,1 do
+		emu.frameadvance();
+	end
 end
  
 -- Sets up the save states
-savestate.save("rayman");
-savestate.load("rayman");
+savestate.save("rayman_ringtest");
+savestate.load("rayman_ringtest");
  
 emu.limitframerate(false);
    
-mycount = 0;
-   
+-- Variables
+x_start = 3260;  
+x_end = 3450;
+y_start = 415;
+y_end = 580;
+
+x_inc = 1;
+y_inc = 1;
+
 -- Main testing loop
-for i=0,NUM_ITER,1 do
--- Grab a random value to set the RNG to; adjust size as necessary
-   for k = 415, 580, 1 do
-   for j = 3250,3450,1 do 
-	savestate.load("rayman"); 
-	set_Y(k)
-    set_X(j);
-	advance_frames(2)
-	check_cond();
-	emu.yield()
+for x = x_start, x_end, x_inc do
+	for y = y_start, y_end, y_inc do 
+		savestate.load("rayman_ringtest"); 
+		set_xy(x, y);
+		advance_frames(2);
+		check_result();
+		emu.yield();
 	end
 end
-end
-   
+
 emu.limitframerate(true);
