dd7a8c84e93837455aa794d54648caa9cb14d53b
got4n <got4n@hotmail.fr>
2016-11-20 12:35:56 +0100

    better lua script, working now :)

--- RaymanOverlay/RaymanOverlay.lua
+++ RaymanOverlay/RaymanOverlay.lua
@@ -2,20 +2,26 @@
 -- Aims to Display Some Useful Infos
 
 -- Constants
+
+while true do
 local CAGES = memory.read_u32_le(0x1F8138)
 local INVIC = memory.read_s16_le(0x1F61E2)
 local PUNCH = memory.read_u8(0x1D8B34)
-
-while true do
 gui.drawImage("cage3.png", 640, 100, 100, 100) 
 gui.drawText(740, 135, ":"..CAGES);
 gui.drawText(640, 200, "Invincilibty:"..INVIC)
 if PUNCH < 2 then
 	gui.drawImage("punch4.png", 640, 250, 43, 40) 
 else
-if PUNCH > 7 then
+	if PUNCH < 4 then
+		gui.drawImage("punch6.png", 640, 250, 92, 40)
+	else
+if PUNCH > 7 and PUNCH < 10 then
 	gui.drawImage("punch5.png", 640, 250, 43, 40) 
+else
+	gui.drawImage("punch7.png", 640, 250, 92, 40)
+end
+end
 end
 emu.frameadvance();
-end
 end
